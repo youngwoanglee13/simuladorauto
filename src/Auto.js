0e12d2ee0d32ff1cbbfbc7b2547d1350737d5110
younglee <young.lee@ucb.edu.bo>
2022-09-27 01:08:15 -0400

    TDD posicion inicial

--- src/Auto.js
+++ src/Auto.js
@@ -1,8 +1,12 @@
 
 class Auto{
-
+    constructor(){
+        this.x=0;
+        this.y=0;
+        this.direccion='N';
+    }
     ejecutarComando(){
-       
+       return this.x+","+this.y+this.direccion;
      
     }
 
--- src/Auto.spec.js
+++ src/Auto.spec.js
@@ -3,7 +3,7 @@ import ejecutarComando from "./Auto";
 const cD = new ejecutarComando();
 describe("Ejecutar comandos", () => {
     it("Posicion inicial", () => {
-      expect(cD.ejecutarComando()).toEqual(0);
+      expect(cD.ejecutarComando()).toEqual("0,0N");
     });
   
     
